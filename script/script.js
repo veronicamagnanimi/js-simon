748b0520d9fc010e8706f081ca53f922994b1673
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-11 15:38:34 +0100

    timeout wip

--- script/script.js
+++ script/script.js
@@ -1,14 +1,47 @@
 //prelevo l'id degli elementi
 const numberCount = document.getElementById("countdown");
 const messageCount = document.getElementById("first-message");
-console.log(numberCount, messageCount);
+const btnCount = document.getElementById("start-btn");
+console.log(numberCount, messageCount, btnCount);
 const randomCount = document.getElementById("five-numbers");
-console.log(randomCount);
+
+//prelevo l'id degli elementi del form
+const formElem = document.getElementById("form-numbers");
+const btnElem = document.getElementById("btn-value");
+console.log(formElem, btnElem);
 
 
 
 
-//inner html
+
+//innerHTML numeri random
+
+
+//quando clicco sul bottone, parte il countdown
+let counter = 30;
+
+btnCount.addEventListener("click", function() {
+    const intervalId = setInterval(function() {
+        numberCount.innerHTML = counter;
+        counter--;   
+
+        if(counter === 0) {
+            clearInterval(intervalId);
+            btnCount.disabled = true;
+        }
+    })
+    
+}, 1000);
+
+
+//appare il form una volta finito il countdown
+
+
+
+
+
+
+
 
 
 
@@ -21,3 +54,4 @@ for (let i = 0; i < 5; i++) {
     randomNumbers.push(Math.floor(Math.random() * 100 ) + 1);
 }
 console.log(randomNumbers);
+
